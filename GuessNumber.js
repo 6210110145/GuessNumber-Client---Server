4aa00567208001c277ed9043639b80878ccfb641
Teerapat <6210110145@psu.ac.th>
2022-01-23 22:50:44 +0700

    new commit

--- GuessNumber.js
+++ GuessNumber.js
@@ -7,7 +7,7 @@ var db = {}
 net.createServer(function (sock){
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort)
     var state = 0 //idle
-    var current_key = Math.floor(Math.random() * 100) + 1
+    let current_key = Math.floor(Math.random() * 100) + 1
 
     sock.on('data', function(data){
         switch(state){
@@ -18,6 +18,7 @@ net.createServer(function (sock){
                     break
                 }else{
                     sock.write('Not the key ')
+                    state = 0
                 }
             case 1:
                 if(data == 'START'){
@@ -26,20 +27,22 @@ net.createServer(function (sock){
                     break
                 }else{
                     sock.write('Send \"START\" to paly')
+                    state = 1
                 }
-            case 2:
-                try{
-                    let client_num = + data
-                    if(client_num > db[current_key]){
-                        sock.write('Less than!!')
-                    }else if(client_num < db[current_key]){
-                        sock.write('More than!!')
-                    }else if(client_num == db[current_key]){
-                        sock.write('Correct!! Send \"STOP\" to exit ')
-                        state = 3 //wait to exit
-                    }
-                }catch(e){
-                    sock.write('INVALID')
+            case 2:        
+                let client_num = + data
+                if(!db[current_key])
+                    db[current_key] = 0
+
+                if(client_num > db[current_key]){
+                    sock.write('Less than!!')
+                    state = 2
+                }else if(client_num < db[current_key]){
+                    sock.write('More than!!')
+                    state = 2
+                }else if(client_num == db[current_key]){
+                    sock.write('Correct!! Send \"STOP\" to exit ')
+                    state = 3 //wait to exit
                 }
             case 3:
                 if(data == 'STOP'){
@@ -53,6 +56,8 @@ console.log('Server listening on ' + HOST + ':' + PORT);
 
 //client
 var net = require('net')
+var HOST = '127.0.0.1';
+var PORT = 6969;
 
 var client = new net.Socket()
 client.connect(PORT, HOST, function() {
@@ -62,7 +67,6 @@ client.connect(PORT, HOST, function() {
     client.on('data', function(data) {
         client.write('START')
         console.log('Received: ' + data)
-        client.write('9')
     })
 })
 
